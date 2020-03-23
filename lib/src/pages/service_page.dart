658f429c9f6df5dd9f68ea234bab6e1848b3bd57
lecocchicomafi <leandro.cocchi@comafi.com.ar>
2020-03-23 20:59:38 -0300

    Create service page responsive

--- lib/src/pages/service_page.dart
+++ lib/src/pages/service_page.dart
@@ -16,32 +16,53 @@ class _ServicePageState extends State<ServicePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBarCustomer(),
-      body: Column(
-        children: <Widget>[
-          SizedBox(
-            height: 80,
-          ),
-          Text(
-            'Servicios',
-            style: TextStyle(fontSize: 50),
-            textAlign: TextAlign.center,
-          ),
-          SizedBox(
-            height: 140,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+      body: SingleChildScrollView(
+        child: Container(
+          width: double.infinity,
+          height: (_isCellPhoneSize(context))
+              ? null
+              : (MediaQuery.of(context).size.height - kToolbarHeight),
+          child: Column(
             children: <Widget>[
-              _buildItems('assets/consultoria.png', 'CONSULTORÍA EN RRHH', 4),
-              _buildItems('assets/evaluacion.png',
-                  'EVALUACIÓN Y DESARROLLO DE TALENTO', 5),
-              _buildItems('assets/talentos.png', 'SELECCIÓN DE TALENTOS', 6),
+              SizedBox(
+                height: 30,
+              ),
+              Text(
+                'Servicios',
+                style: TextStyle(
+                  fontSize: (_isCellPhoneSize(context)) ? 35 : 60,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(
+                height: (_isCellPhoneSize(context)) ? 20 : 0,
+              ),
+              Expanded(
+                flex: (_isCellPhoneSize(context)) ? 0 : 1,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Wrap(
+                      alignment: WrapAlignment.center,
+                      children: <Widget>[
+                        _buildItems(
+                            'assets/consultoria.png', 'CONSULTORÍA EN RRHH', 4),
+                        _buildItems('assets/evaluacion.png',
+                            'EVALUACIÓN Y DESARROLLO DE TALENTO', 5),
+                        _buildItems(
+                            'assets/talentos.png', 'SELECCIÓN DE TALENTOS', 6),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
-        ],
+        ),
       ),
     );
   }
+
 
   Widget _buildItems(String path, String text, int page) {
     return GestureDetector(
@@ -52,7 +73,7 @@ class _ServicePageState extends State<ServicePage> {
       child: Container(
         width: 300,
         height: 150,
-        margin: EdgeInsets.symmetric(horizontal: 25),
+        margin: EdgeInsets.all(25),
         padding: EdgeInsets.symmetric(horizontal: 15),
         color: Colors.grey[200],
         child: Column(
@@ -71,4 +92,7 @@ class _ServicePageState extends State<ServicePage> {
       ),
     );
   }
+
+  bool _isCellPhoneSize(BuildContext context) =>
+      MediaQuery.of(context).size.width < 1000;
 }
