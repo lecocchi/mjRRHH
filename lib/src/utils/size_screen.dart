6176173d1575cc178cf7c8a95810c2576ca349f6
lecocchicomafi <leandro.cocchi@comafi.com.ar>
2020-04-12 17:14:11 -0300

    create actions github

---
+++ lib/src/utils/size_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class SizeScreen extends StatelessWidget {
+  final phone;
+  final tablet;
+  final desktop;
+  final smart;
+
+  const SizeScreen(
+      {this.phone,
+      this.tablet,
+      this.desktop,
+      this.smart});
+
+  @override
+  Widget build(BuildContext context) {
+    if (_isPhone(context)) return this.phone ?? Container();
+    if (_isTablet(context)) return this.tablet ?? Container();
+    if (_isDesktop(context)) return this.desktop ?? Container();
+    if (_isSmart(context)) return this.smart ?? Container();
+    return Container();
+  }
+
+  bool _isPhone(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    return (width >= 320 && width < 720);
+  }
+
+  bool _isTablet(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    return (width >= 720 && width < 1024);
+  }
+
+  bool _isDesktop(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    return (width >= 1024 && width < 2560);
+  }
+
+  bool _isSmart(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    return (width >= 2560);
+  }
+}
